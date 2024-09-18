bc78a05bcb282957f0f4b883309f3e2329060591
Jasmin Kozlica <Jasminkozlica20@gmail.com>
2024-09-18 16:41:08 +0200

    database created

---
+++ src/main/java/Journal.java
@@ -0,0 +1,13 @@
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class Journal {
+    private Long id;
+    private String name;
+
+}
+
---
+++ src/main/java/LearningJournalController.java
@@ -0,0 +1,9 @@
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class LearningJournalController {
+    public Journal personJson(){
+
+    }
+}
+//Endpunkte für datenbank
