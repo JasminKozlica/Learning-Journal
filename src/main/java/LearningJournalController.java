08ae6c532bc7a0be230fd43c614cb6eafcbdce9c
Jasmin Kozlica <Jasminkozlica20@gmail.com>
2024-09-20 10:26:27 +0200

    wird weniger rot

--- src/main/java/LearningJournalController.java
+++ src/main/java/LearningJournalController.java
@@ -1,5 +1,6 @@
 import com.example.demo.Journal;
 import com.example.demo.JournalService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,14 +10,15 @@ import java.util.List;
 
 @Controller
 public class LearningJournalController {
-    public Journal personJson(){
 
+    @Autowired
+    private JournalService journalService;
 
-        @GetMapping (value ="/journal")
-                @ResponseBody
-        public List<Journal>getJournal(){
-            return JournalService.getAll();
-        }
-        @GetMapping(value = "/journal/")
+    @GetMapping(value = "/journal")
+    @ResponseBody
+    public List<Journal> getJournal() {
+        return journalService.getAllEntries();
     }
+
+   // @GetMapping(value = "/journal/")
 }
--- src/main/java/com/example/demo/JournalRepository.java
+++ src/main/java/com/example/demo/JournalRepository.java
@@ -3,10 +3,11 @@ package com.example.demo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface JournalRepository extends JpaRepository<JournalEntry,Long> {
-    List<Journal>findByCategory(String category);
-    Optional<Journal> findById(Long entryId);
+public interface JournalRepository extends JpaRepository<Journal,Long> {
+    List<Journal> findByCategory(String category);
+
 }
